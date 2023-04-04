bcea4e66d95b03ca8b1e448646647844cc63ca62
Savage Blood <malifarge.thomas9@gmail.com>
2023-04-04 11:08:07 +0200

    setupAdmin

--- src/API/Supabase-FLX/User.tsx
+++ src/API/Supabase-FLX/User.tsx
@@ -3,7 +3,7 @@ import supabase from "./client"
 export const GetIds = async() =>{
     let { data: Identifiant, error } = await supabase.from('Identifiant').select('id,COM_ACTnom')
     if(error){
-        console.log(error); 
+        console.log(error.details); 
     }else{
         return Identifiant
     }
