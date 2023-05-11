b0a942925889abd608dcb71b46d2adcff6559da1
Savage Blood <malifarge.thomas9@gmail.com>
2023-05-11 15:18:50 +0200

    Correction

--- src/Components/Evenement.tsx
+++ src/Components/Evenement.tsx
@@ -1,30 +1,21 @@
+
 import { useEffect, useState } from "react"
 import { Evenement } from "../Types/Evenement"
 import { donwload } from "../API/Supabase/Images"
 import { EvenementContain } from "../Styles/Evenement"
-import { GetVillage } from "../API/Supabase/Village"
 
 
 export const EvenementList = ({evenement}:Evenement) =>{
 
     const [image,setImage] = useState<string>('')
-    const [village,setVillage]=useState<string>('')
 
     useEffect(()=>{
         if(evenement.Image){
             const data =  donwload(evenement.Image)
             setImage(data.publicUrl)
         }
-            FetchVillage()
     },[evenement])
     
-
-    const FetchVillage = async()=>{
-        if(evenement.villageId){
-            const Village = await GetVillage(evenement.villageId) as any
-            setVillage(Village[0].nomVillage)
-        }
-    }
 
     return <EvenementContain backgroundUrl={image} Foncé={evenement.nom!== null}>
         {evenement.nom && <h3>{evenement.nom}</h3>}
