f2c3a793520ab1111503422ffe9d743ca5ba98c4
Savage Blood <malifarge.thomas9@gmail.com>
2023-05-12 14:44:03 +0200

    correction evenementList

--- src/Components/Evenement.tsx
+++ src/Components/Evenement.tsx
@@ -3,11 +3,14 @@ import { useEffect, useState } from "react"
 import { Evenement } from "../Types/Evenement"
 import { donwload } from "../API/Supabase/Images"
 import { EvenementContain } from "../Styles/Evenement"
+import { useNavigate } from "react-router-dom"
 
 
 export const EvenementList = ({evenement}:Evenement) =>{
 
     const [image,setImage] = useState<string>('')
+
+    const navigate= useNavigate()
 
     useEffect(()=>{
         if(evenement.Image){
@@ -15,9 +18,13 @@ export const EvenementList = ({evenement}:Evenement) =>{
             setImage(data.publicUrl)
         }
     },[evenement])
+
+    const handleEvenementClick= ()=>{
+        navigate(`/Evenement/${evenement.id}`)
+    }
     
 
-    return <EvenementContain backgroundUrl={image} Foncé={evenement.noAffiche}>
+    return <EvenementContain backgroundUrl={image} Foncé={evenement.noAffiche} onClick={handleEvenementClick}>
         {evenement.noAffiche && <><h3>{evenement.nom}</h3>
         <div className="Description">
             {evenement.description.split('\n').map((line,id)=>{if(line===""){return <br key={id}/>}else{return <p key={id}>{line}</p>}})}
--- src/Styles/Evenement.tsx
+++ src/Styles/Evenement.tsx
@@ -10,6 +10,7 @@ export const EvenementContain = styled.div`
     flex-direction:column;
     align-items:center;
     justify-content:space-around;
+    cursor:pointer;
 
     p{
         border:none;
