0aa28dc5d741c9498f11ead6e123bc6c128a80ea
Savage Blood <malifarge.thomas9@gmail.com>
2023-06-20 15:24:42 +0200

    test demande autorisation son

--- src/Components/Audio.tsx
+++ src/Components/Audio.tsx
@@ -1,18 +1,41 @@
-import { useEffect, useRef } from "react"
+import React, { useEffect, useRef } from "react";
 
-export const Audio = ({audioUrl}:any) =>{
+export const Audio = ({ audioUrl }: any) => {
+  const musicRef = useRef(null) as any;
 
-    const musicRef= useRef(null) as any
+  useEffect(() => {
+    const playAudio = async () => {
+      try {
+        await musicRef.current.play();
+      } catch (error) {
+        // La lecture automatique a été bloquée
+        console.log("La lecture automatique du son est bloquée par le navigateur.");
+      }
+    };
 
-    useEffect(()=>{
-        play()
-    },[])
+    const requestAudioPermission = async () => {
+      try {
+        await musicRef.current.play();
+        await musicRef.current.pause();
+      } catch (error) {
+        // La demande de lecture a été bloquée ou l'utilisateur a refusé
+        console.log("L'utilisateur a refusé la lecture automatique du son.");
+      }
+    };
 
-    const play = () =>{
-        musicRef.current.play()
-    }
-    
-    return(
-        <audio ref={musicRef} controls src={audioUrl}/>
-    )
-}
+    const element = musicRef.current;
+
+    // Demander la permission à l'utilisateur lorsqu'il arrive sur la page
+    document.addEventListener("DOMContentLoaded", requestAudioPermission);
+
+    // Lancer automatiquement la lecture audio une fois la permission accordée
+    element.addEventListener("canplaythrough", playAudio);
+
+    return () => {
+      document.removeEventListener("DOMContentLoaded", requestAudioPermission);
+      element.removeEventListener("canplaythrough", playAudio);
+    };
+  }, []);
+
+  return <audio controls ref={musicRef} src={audioUrl} />;
+};
