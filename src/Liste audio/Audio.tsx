5e4fbdd9b9dfe14cbd5adee888003d8763e73827
Savage Blood <malifarge.thomas9@gmail.com>
2023-06-21 12:01:22 +0200

    rajout audios

--- src/Liste audio/Audio.tsx
+++ src/Liste audio/Audio.tsx
@@ -2,5 +2,50 @@ export const AudioList =[
     {
         AudioId:"1",
         AudioUrl:'Voix002.m4a'
-    }
+    },
+
+    {
+        AudioId:"2",
+        AudioUrl:'Eileen(tristesse).m4a'
+    },
+
+    {
+        AudioId:"3",
+        AudioUrl:'Eva.m4a'
+    },
+
+    {
+        AudioId:"4",
+        AudioUrl:'Inès.m4a'
+    },
+
+    {
+        AudioId:"5",
+        AudioUrl:'Louise(jacqueline).m4a'
+    },
+
+    {
+        AudioId:"6",
+        AudioUrl:'Lya(micheline).m4a'
+    },
+
+    {
+        AudioId:"7",
+        AudioUrl:'Tyméo(Bertrand).m4a'
+    },
+
+    {
+        AudioId:"8",
+        AudioUrl:'Tous_ensemble(Lya,Louise,Eva,Tyméo,eileen,Ines).m4a'
+    },
+
+    {
+        AudioId:"9",
+        AudioUrl:'Tous_ensemble_2.m4a'
+    },
+
+    {
+        AudioId:"10",
+        AudioUrl:'Tous_ensemble_3.m4a'
+    },
 ]
