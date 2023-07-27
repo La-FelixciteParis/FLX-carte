c0302c7824002bd13eb7c65d21eab05cd5205115
Savage Blood <malifarge.thomas9@gmail.com>
2023-07-27 10:54:47 +0200

    comment header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
+//Header à refaire pour avoir une nav complête
+
+
+//Import
+
+//Base
 import { useContext, useEffect, useState } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
+
+//Context
 import { CommerçantContext } from "../Context/Commercant"
 import { UserContext } from "../Context/IdUser"
+
+//Style
 import { bleu, jaune, Principal, vert } from "../Styles/Couleur"
 import { ButtonStyle } from "../Styles/Général"
 import { HeaderStyle } from "../Styles/Header"
@@ -25,6 +35,8 @@ export const Header = ()=>{
 
 
     useEffect(()=>{
+
+        //Choix d'une couleur en fonction du type d'utilisateur
         if(infoIdUser){
             const carte = infoIdUser[1]
             if (carte==="ACT") {
@@ -40,31 +52,39 @@ export const Header = ()=>{
     },[infoIdUser])
 
     const logout = () =>{
+
+        //Déconection en remettant sur la homepage
         logoutCommerçant()
         logoutId()
         navigate("/")
     }
 
     const handleConnexionCommerçant = ()=>{
+
+        //Renvoie vers la page de log d'un commerçant
         navigate("/Login")
     }
 
     const handleHomeClick = () =>{
+        //renvoie à la homepage
         navigate("/")
     } 
 
+    //Récupération du chemin de la page
     const location= useLocation()
 
     useEffect(()=>{
-        
+        //Si le chemin est un /Commerce (donc on est sur la page commerce)...
         if(location.pathname.split("/")[1]=== "Commerce"){
             const handleScroll = () => {
                 setScrollTop(window.scrollY);
               };
           
+            //... on regarde le scroll de la page et on le garde en scrollTop
           window.addEventListener("scroll", handleScroll);
         };
-    
+
+        //Si le chemin est différent de Admin on efface les acces Admin du storage local
         if(location.pathname.split("/")[1]!=='Admin'){
             localStorage.removeItem('AdminPerm')
         }
@@ -74,10 +94,11 @@ export const Header = ()=>{
     
 
     useEffect(()=>{
-        
+        //Quand le scrollTop change
+        //on récupère la hauteur de la page
         const height = window.innerHeight
         
-        
+        //On compare le scrollTop garder avec la hauteur si on est sur la page commerce et on met le scroll à true ou false en fonction de cette valeur
         if(scrollTop>=height-70 || location.pathname.split("/")[1]!== "Commerce"){
             setScroll(true);   
         }else{
@@ -87,6 +108,8 @@ export const Header = ()=>{
     },[scrollTop])
 
     return(
+
+        //le style du header change suivant si scroll est true ou false
         <HeaderStyle color={color} headerAppear={scroll} bright={scroll ? 0.5 : 3}>
             <img className="logo" src="/Images/LogoEtoileFlx.png" alt="Logo Félixcité" onClick={handleHomeClick}/>
             <img className="anime" src="/Images/éclat.png" alt="éclat"/>
