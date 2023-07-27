d15ebc5db9894a0f4612351a0c862e458e2d1550
Savage Blood <malifarge.thomas9@gmail.com>
2023-07-27 11:36:35 +0200

    comment context commercant

--- src/Context/Commercant.tsx
+++ src/Context/Commercant.tsx
@@ -1,7 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
+//Context pour garder un commerçant connecté (l'idée étant qu'il puisse lire des cartes perso pour possiblement de futur actions.
+//Donc on garde le commerçant et au lieu d'une connection perso on à les actions voulue (genre fidélité))
+
+//Import
+
+//Base
 import { createContext, useEffect, useState } from "react";
+
+//API
 import { GetACT } from "../API/Supabase/User";
+
+//Type
 import { ChildrenPropsType } from "../Types/Children";
 
 const CommerçantContext = createContext({})
@@ -11,6 +21,7 @@ const CommerçantContextProvider = ({children}:ChildrenPropsType) =>{
     const [token,setToken] = useState<string>('')
 
     useEffect(()=>{
+        //Si il y à déjà un token dans le storage local on le prend
         const localToken = localStorage.getItem('TokenCommerçantFLX')
         if(localToken){
             setToken(localToken)
@@ -18,6 +29,8 @@ const CommerçantContextProvider = ({children}:ChildrenPropsType) =>{
     },[])
 
     useEffect(()=>{
+
+        //Quand il y à un token on le met en storage et on récupère les données
         if(token){
             localStorage.setItem('TokenCommerçantFLX',token)
             ACT()
@@ -25,7 +38,7 @@ const CommerçantContextProvider = ({children}:ChildrenPropsType) =>{
     },[token])
 
     const ACT = async() =>{
-            
+            //récupération des données
             const data = await GetACT(token)
             if(data){
                 setUser(data)
@@ -36,6 +49,8 @@ const CommerçantContextProvider = ({children}:ChildrenPropsType) =>{
     }
 
     const logoutCommerçant = ()=>{
+
+        //déco du commerçant en supprimant tout
         setUser(null)
         setToken('')
         localStorage.removeItem('TokenCommerçantFLX')
